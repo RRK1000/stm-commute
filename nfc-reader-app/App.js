af4476593b717e238f0cfc4daba6b98c0cd17e66
Rishi Ravikumar <rishiravi.k98@gmail.com>
2024-01-20 16:04:10 -0500

    feat: update App.js

--- nfc-reader-app/App.js
+++ nfc-reader-app/App.js
@@ -1,10 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import NfcManager, { NfcEvents } from 'react-native-nfc-manager';
+
+
+NfcManager.start();
 
 export default function App() {
+  async function scanTag() {
+      NfcManager.setEventListener(NfcEvents.DiscoverTag, (tag) => {
+        console.log("tag found", tag);
+      })
+
+    await NfcManager.registerTagEvent();
+  }
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <TouchableOpacity onPress={scanTag}>
+        <Text>Scan a Tag</Text>
+      </TouchableOpacity>
       <StatusBar style="auto" />
     </View>
   );
