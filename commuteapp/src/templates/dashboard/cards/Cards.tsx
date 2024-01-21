4c60e6f674c67223280471e84361fe447c4be4c7
rkerwi <rowan.kerwin@mail.mcgill.ca>
2024-01-21 05:40:54 -0500

    reload functionality

--- commuteapp/src/templates/dashboard/cards/Cards.tsx
+++ commuteapp/src/templates/dashboard/cards/Cards.tsx
@@ -1,37 +1,50 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text, StyleSheet, ScrollView } from 'react-native';
+import { View, StyleSheet, ScrollView, RefreshControl } from 'react-native';
 import PocketBase from 'pocketbase'
 import Card from './Card'
 
 const url = 'https://stm-commute.pockethost.io/'
 const pb = new PocketBase(url)
 
-
 const PocketBaseCardsList: React.FC = () => {
+
     const [cardsData, setCardsData] = useState<any[]>([]); // Assuming any data structure
 
+    const fetchData = async () => {
+        try {
+            await pb.admins.authWithPassword('admin@admin.com', 'admin123123');
+
+            const user = await pb.collection('users').getFirstListItem('name="rrk"', {
+                expand: 'cards',
+            });
+            const userJson = JSON.parse(JSON.stringify(user))
+            setCardsData(userJson["cards"]);
+        } catch (error) {
+            console.error('Error fetching data:', error);
+        }
+        
+    };
+
     useEffect(() => {
-        const fetchData = async () => {
-            try {
-                await pb.admins.authWithPassword('admin@admin.com', 'admin123123');
-
-                const user = await pb.collection('users').getFirstListItem('name="rrk"', {
-                    expand: 'cards',
-                });
-                const userJson = JSON.parse(JSON.stringify(user))
-                setCardsData(userJson["cards"]);
-            } catch (error) {
-                console.error('Error fetching data:', error);
-            }
-            
-        };
-
         fetchData();
     }, []); // Empty dependency array to fetch data only once on mount
 
+    const [refreshing, setRefreshing] = React.useState(false);
+    const onRefresh = React.useCallback(() => {
+        setRefreshing(true);
+        fetchData();
+        setTimeout(() => {
+          setRefreshing(false);
+        }, 1000);
+      }, []);
+
     return (
         <View style={styles.container}>
-            <ScrollView showsVerticalScrollIndicator={false} contentContainerStyle={styles.cardsContainer}>
+            <ScrollView showsVerticalScrollIndicator={false} 
+            contentContainerStyle={styles.cardsContainer}
+            refreshControl={
+                <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
+              }>
                 {cardsData.map((card, index) => (
                     <View key={index} style={styles.card}>
                         <Card
--- commuteapp/src/templates/dashboard/profile/UserProfile.tsx
+++ commuteapp/src/templates/dashboard/profile/UserProfile.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { SafeAreaView, View, Text } from "react-native";
+import { SafeAreaView, View, Text, ScrollView, RefreshControl } from "react-native";
 import tailwind from "twrnc";
 import { Avatar } from 'react-native-elements';
 import PocketBase from 'pocketbase'
@@ -14,44 +14,59 @@ interface User {
 }
 
 const UserProfile: React.FC = () => {
+
+    const [refreshing, setRefreshing] = React.useState(false);
+    const onRefresh = React.useCallback(() => {
+        setRefreshing(true);
+        fetchData();
+        setTimeout(() => {
+          setRefreshing(false);
+        }, 1000);
+      }, []);
+
     const [userData, setUserData] = useState<any>(); // Assuming any data structure
 
+    const fetchData = async () => {
+        try {
+            await pb.admins.authWithPassword('admin@admin.com', 'admin123123');
+
+            const pbuser = await pb.collection('users').getFirstListItem('name="rrk"', {
+                expand: 'cards',
+            });
+
+            const userJson = JSON.parse(JSON.stringify(pbuser))
+
+            const user: User = {
+                name: userJson.name,
+                age: userJson.age,
+                email: userJson.email,
+            };
+            setUserData(user);
+        } catch (error) {
+            console.error('Error fetching data:', error);
+        }
+    };
+
     useEffect(() => {
-        const fetchData = async () => {
-            try {
-                await pb.admins.authWithPassword('admin@admin.com', 'admin123123');
-
-                const pbuser = await pb.collection('users').getFirstListItem('name="rrk"', {
-                    expand: 'cards',
-                });
-
-                const userJson = JSON.parse(JSON.stringify(pbuser))
-
-                const user: User = {
-                    name: userJson.name,
-                    age: userJson.age,
-                    email: userJson.email,
-                };
-                setUserData(user);
-            } catch (error) {
-                console.error('Error fetching data:', error);
-            }
-        };
-
         fetchData();
     }, []); // Empty dependency array to fetch data only once on mount
     return (
-        <SafeAreaView style={tailwind`w-full flex-1 bg-gray-50`}>
-
-            <View style={tailwind`flex-1 items-center justify-center gap-8`}>
-                <View style={tailwind`gap-2 items-center`}>
-                    <Text style={tailwind`text-gray-950 text-3xl font-bold`}>
-                        {userData ? userData.name : ""}
-                    </Text>
-                    <Text style={tailwind`text-gray-950 text-lg`}>{userData ? userData.email : ""}</Text>
+        <ScrollView 
+            showsVerticalScrollIndicator={false} 
+        refreshControl={
+            <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
+          }>
+            <SafeAreaView style={tailwind`w-full flex-1 bg-gray-50`}>
+                <View style={tailwind`flex-1 items-center justify-center gap-8`}>
+                    <View style={tailwind`gap-2 items-center`}>
+                        <Text style={tailwind`text-gray-950 text-3xl font-bold`}>
+                            {userData ? userData.name : ""}
+                        </Text>
+                        <Text style={tailwind`text-gray-950 text-lg`}>{userData ? userData.email : ""}</Text>
+                    </View>
                 </View>
-            </View>
-        </SafeAreaView>
+            </SafeAreaView>
+          </ScrollView>
     );
 };
 export default UserProfile;
